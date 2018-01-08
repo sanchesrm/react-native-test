b1bc858d7a701dfcef070677bf75581c95b328e3
Rodrigo Sanches <sanchesrm@hotmail.com>
2018-01-08 00:04:39 -0200

    router.js

--- router.js
+++ router.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { StackNavigator } from 'react-navigation';
+import { DrawerNavigator, StackNavigator } from 'react-navigation';
 import AgendaScreen from './Components/Agenda';
 
+const DrawerStack = DrawerNavigator({
+  screen: { AgendaScreen: AgendaScreen }
+}, {
+  initialRouteName: 'AgendaScreen',
+  // contentComponent: Menu,
+  headerMode: 'float',
+  navigationOptions: ({navigation}) => ({
+    headerStyle: {backgroundColor: '#4C3E54'},
+    title: 'Welcome!',
+    headerTintColor: 'white',
+  })
+});
+
 const RootNavigator = StackNavigator({
-  Home: {
-    screen: AgendaScreen,
-    navigationOptions: {
-    	title: 'Home Screen'
-    }
-  }
+  // Home: {
+  //   screen: AgendaScreen,
+  //   navigationOptions: {
+  //   	title: 'Home Screen'
+  //   }
+  // },
+  drawerStack: {DrawerStack}
 });
 
 export default RootNavigator;
