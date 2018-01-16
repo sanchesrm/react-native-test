86b01fc3e233c742c656b54d88b704c0bdac49ff
Rodrigo Sanches <sanchesrm@hotmail.com>
2018-01-15 23:05:56 -0200

    15/01

--- Components/AddCompromisso.js
+++ Components/AddCompromisso.js
@@ -3,10 +3,13 @@ import {
   Text,
   View,
   TextInput,
-  StyleSheet
+  StyleSheet,
+  Dimensions
 } from 'react-native';
-import { FormLabel, FormInput } from 'react-native-elements';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
+
+var width = Dimensions.get('window').width; 
 
 class AddCompromisso extends Component {
   render() {
@@ -15,8 +18,10 @@ class AddCompromisso extends Component {
         <FormLabel>Class</FormLabel>
         <FormInput placeholder="Type the class"/>
 
-        <DatePicker mode="date" style={{ flex:1 }} />
-        <DatePicker mode="time" format="HH:mm" />
+        <DatePicker mode="date" style={ styles.datePicker } />
+        <DatePicker mode="time" format="HH:mm" style={ styles.datePicker } />
+
+        <Button title='Add New Class' style={ styles.submitButton } />
       </View>
     );
   }
@@ -25,7 +30,18 @@ class AddCompromisso extends Component {
 const styles = StyleSheet.create({
   container: {
     marginTop: 50, 
-    padding: 20
+    padding: 20, 
+    flex: 1
+  }, 
+  datePicker: {
+    width: width, 
+    paddingTop: 20,
+    paddingRight: 50, 
+    paddingLeft: 20 
+  }, 
+  submitButton: {
+    position: 'absolute',
+    bottom: 50
   }
 });
 
